838a6a92f441abf8e91be88eefdf412682dca4d4
Anton Gulkevich <ycykensmit@gmail.com>
2015-10-14 14:43:39 +0300

    added function zip

--- Configuration_Viewer/Configuration_Viewer/StrategyDeployment .cpp
+++ Configuration_Viewer/Configuration_Viewer/StrategyDeployment .cpp
@@ -1,4 +1,8 @@
 ﻿#include "StrategyDeployment .h"
+#include <windows.h>
+#include <stdio.h>
+#include <tchar.h>
+#include <iostream>
 
 StrategyDeployment::StrategyDeployment(const std::string commodFileName_) :
 	commodFileName(commodFileName_)
@@ -8,13 +12,41 @@ StrategyDeployment::StrategyDeployment(const std::string commodFileName_) :
 	saveToSameFolder = true;
 	zipCompressionLevel = 7;
 	createCompressedFiled = false;
-	zipLocation = "undefined";
+	zipLocation = "D:\\Program Files (x86)\\7-Zip\\7z.exe";
 
 }
 
 bool StrategyDeployment::zip(const std::string& sourceFileName, const std::string zippedFileName)
 {
-		
+	STARTUPINFOA si;
+	PROCESS_INFORMATION pi;
+	ZeroMemory(&si, sizeof(si));
+	si.cb = sizeof(si);
+	ZeroMemory(&pi, sizeof(pi));
+	auto appParam = "7z a -tzip -mx7 \"" + zippedFileName +"\"" + " \"" + sourceFileName + "\"";	
+	if (!CreateProcessA(zipLocation.c_str(),
+		_strdup(appParam.c_str()),
+		nullptr,           // Process handle not inheritable
+		nullptr,           // Thread handle not inheritable
+		FALSE,			   // Set handle inheritance to FALSE
+		CREATE_NO_WINDOW,      
+		nullptr,           // Use parent's environment block
+		nullptr,           // Use parent's starting directory 
+		&si,               // Pointer to STARTUPINFO structure
+		&pi)               // Pointer to PROCESS_INFORMATION structure
+		)
+	{
+		std::cout<<"CreateProcess failed "<< GetLastError()<< std::endl;
+		return false;
+	}
+
+	// Wait until child process exits.
+	WaitForSingleObject(pi.hProcess, INFINITE);
+
+	// Close process and thread handles. 
+	CloseHandle(pi.hProcess);
+	CloseHandle(pi.hThread);
+	return true;
 }
 
 void StrategyDeployment::execute()
--- Configuration_Viewer/Configuration_Viewer/StrategyDeployment .h
+++ Configuration_Viewer/Configuration_Viewer/StrategyDeployment .h
@@ -1,6 +1,7 @@
 ﻿#pragma once
 
 #include <string>
+#include <vector>
 class StrategyDeployment 
 {
 private:
@@ -10,28 +11,48 @@ private:
 	bool parseEnabled;
 	bool zipEnabled;
 	bool saveToSameFolder;
-	short zipCompressionLevel;
+	bool createCompressedFiled;
+	short zipCompressionLevel;//1-9
+
+	void * hInstDll;
+	std::vector <std::string *> logVector;
+
+	struct Fat {
+		unsigned int address;
+		unsigned int size;
+	};
 public:
 	explicit StrategyDeployment(const std::string commodFileName_);
 	//get
 	std::string getCommodFileName()	{return commodFileName;}
-	short getZipCompressionLevel() { return zipCompressionLevel; }
-	std::string getZipLocation() { return zipLocation; }
-	bool isParseEnabked() { return parseEnabled; }
-	bool isZipEnabled() { return zipEnabled; }
-	bool isSaveToSameFolder() { return saveToSameFolder; }
+	short getZipCompressionLevel() const { return zipCompressionLevel; }
+	std::string getZipLocation() const  { return zipLocation; }
+	bool isParseEnabked() const  { return parseEnabled; }
+	bool isZipEnabled() const  { return zipEnabled; }
+	bool isSaveToSameFolder() const  { return saveToSameFolder; }
+	bool isCreateCompressedFile()const { return createCompressedFiled; }
 	
-
 	//set
 	void setZipLocation(const std::string & location) { zipLocation = location; }
 	void setzipCompressionLevel(short level) { zipCompressionLevel = level; }
 	void setParse(bool state) { parseEnabled = state;}
 	void setZip(bool state) { zipEnabled = state; }
 	void zetSaveToSameFolder(bool state) { saveToSameFolder = state; }
+	void setCreateCompressedFile(bool state) { createCompressedFiled = state; }
 
+	//compression
+	bool zip(const std::string &sourceFileName, const std::string zippedFileName);
+	bool unzip();
 
-	~StrategyDeployment(){};
+	//operations with files
+	bool saveFile();
+	bool openfile();
+	
+	//log
+	bool saveLog();
+
 	void execute();
+	~StrategyDeployment(){};	
 };
 
 
